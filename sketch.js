476e41ea7b6ea45f37f86d7306e676d7730a424f
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-11-02 11:10:49 -0500

    spawn, draw and move aliens

---
+++ sketch.js
@@ -0,0 +1,42 @@
+//create array
+let aliens = []
+
+function setup() {
+  createCanvas(400, 400);
+  spawnAliens()
+  console.log(aliens)
+}
+
+function draw() {
+  background(220);
+  for (let i=0;i<10;i++) {
+    updateSprite(aliens[i])
+    drawAlien(aliens[i])
+  }
+}
+
+function updateSprite(s) {
+  s.x += s.vx
+  s.y += s.vy
+}
+
+function drawAlien(a) {
+  fill("green")
+  circle(a.x,a.y,a.width)
+}
+
+function spawnAliens() {
+  
+  for (let i=0;i<10;i++) {
+    let alien = {}
+    alien.x = i * 32
+    alien.y = 10
+    alien.vx = 1
+    alien.vy = 0.5
+    alien.width = 30
+    alien.height = 30
+    //add this alien to array
+    aliens.push(alien)
+  }
+  
+}
