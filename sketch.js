f9a7bc9b5a82d511519c93f3955a2ca71b18bcec
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-11-05 11:20:17 -0500

    aliens move, bounce, gameOver

--- sketch.js
+++ sketch.js
@@ -9,15 +9,13 @@ function setup() {
 
 function draw() {
   background(220);
+  
   for (let i=0;i<10;i++) {
-    updateSprite(aliens[i])
     drawAlien(aliens[i])
+    alienVsWalls(aliens[i])
+    updateSprite(aliens[i])
+    
   }
-}
-
-function updateSprite(s) {
-  s.x += s.vx
-  s.y += s.vy
 }
 
 function drawAlien(a) {
@@ -25,18 +23,33 @@ function drawAlien(a) {
   circle(a.x,a.y,a.width)
 }
 
+function updateSprite(s) {
+  s.x += s.vx
+  s.y += s.vy
+}
+
+function alienVsWalls(a) {
+  if (a.x > width || a.x < 0 ) {
+    a.vx *= -1
+  }
+  else if (a.y > height) {
+    console.log("You lose!")
+    noLoop()
+  }
+}
+
 function spawnAliens() {
-  
   for (let i=0;i<10;i++) {
     let alien = {}
-    alien.x = i * 32
-    alien.y = 10
-    alien.vx = 1
-    alien.vy = 0.5
+    alien.x = i * 33 + 18
+    alien.y = 18
     alien.width = 30
     alien.height = 30
-    //add this alien to array
+    alien.vx = 3
+    alien.vy = 0.2
+    //add new alien to array
     aliens.push(alien)
   }
-  
 }
+
+
