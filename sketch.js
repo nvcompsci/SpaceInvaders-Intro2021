a79108cfa6a285e22593dac3d4e0275cf844af9a
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-11-06 09:45:07 -0500

    start collision

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 //create array
 let aliens = []
+let lasers = []
 
 function setup() {
   createCanvas(400, 400);
@@ -9,26 +10,49 @@ function setup() {
 
 function draw() {
   background(220);
-  for (let i=0;i<10;i++) {
+  for (let i = 0; i < 10; i++) {
     updateSprite(aliens[i])
     alienVsWalls(aliens[i])
     drawAlien(aliens[i])
+  }
+  for (let i = 0; i < lasers.length; i++) {
+    updateSprite(lasers[i])
+    drawLaser(lasers[i])
   }
 }
 
 function alienVsWalls(a) {
   if (a.x > width || a.x < 0) {
     a.vx *= -1
-  }
-  else if (a.y > height) {
+  } else if (a.y > height) {
     console.log("You lose!")
     noLoop()
   }
 }
 
+function laserVsAlien(l, a) {
+  if (3 > 2) {
+
+  }
+}
+
+function rectVsRect(rect1, rect2) {
+  if (rect1.x < rect2.x + rect2.width &&
+    rect1.x + rect1.width > rect2.x &&
+    rect1.y < rect2.y + rect2.height &&
+    rect1.y + rect1.height > rect2.y) {
+    return true;
+  } else return false;
+}
+
 function drawAlien(a) {
   fill("green")
   circle(a.x, a.y, a.width)
+}
+
+function drawLaser(l) {
+  fill("red")
+  rect(l.x, l.y, l.width, l.height)
 }
 
 function updateSprite(s) {
@@ -37,19 +61,31 @@ function updateSprite(s) {
 }
 
 function spawnAliens() {
-  for (let i=0; i<10;i++) {
+  for (let i = 0; i < 10; i++) {
     let alien = {}
     alien.x = (i * 33) + 16
     alien.y = 16
     alien.width = 30
     alien.height = 30
-    alien.vx = 2
+    alien.vx = 3
     alien.vy = 0.2
     //put new alien in array
     aliens.push(alien)
   }
 }
 
+function keyReleased() {
+  fireLaser()
+}
 
+function fireLaser() {
+  let laser = {}
+  laser.x = width / 2
+  laser.y = height - 10
+  laser.width = 2
+  laser.height = 10
+  laser.vx = 0
+  laser.vy = -5
 
-
+  lasers.push(laser)
+}
